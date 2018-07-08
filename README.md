b36b5dec8f392399256c2baf818a1fa2b2b75528
Steven Fang <fondoger@outlook.com>
2018-07-08 10:57:49 +0800

    adjust code style

--- README.md
+++ README.md
@@ -11,13 +11,19 @@
 ```
 # 1. 安装Requests
 pip install requests（可能需要管理员权限）
-# 1. 安装BeautifulSoup4
-pip install lxml (可能需要管理员权限）
-# 2. 安装lxml
-pip install lxml（可能需要管理员权限）
+# 2. 安装BeautifulSoup4
+pip install beautifulsoup4 (可能需要管理员权限）
 # 3. 运行
 python main.py
 ```
 
-**实现功能**
+**功能简介**
 
+1. 自动选择最佳评价（由于至少需要有一个选项与其他不同，故第一项选择为良，其他全部选择优秀）
+
+2. 统一认证自动登陆（提供学号、密码，一键傻瓜式操作）
+
+
+**其他问题**
+
+由于本脚本仅在本机测试（windows 10）通过，无法保证其他环境下能正常使用。如有任何问题，欢迎issue中交流。
--- teacher_judge.py
+++ teacher_judge.py
@@ -3,20 +3,6 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 
 
-session = requests.Session()
-session.headers = {
-    'Cookie': 'JSESSIONID=2f88b1mLwrV8Yx9SyDh1yLJQzQJzR1HF9qPCKPql63LMTXfgpnl5!-822511665'
-}
-res = session.get("http://10.200.21.61:7001/ieas2.1/xspj/Fxpj_fy", allow_redirects=False)
-if res.status_code != 200:
-    print("can't load page")
-    exit()
-
-soup = BeautifulSoup(res.content, 'html.parser')
-yellow_spans = soup.find_all('span', class_='yellow')
-teachers = []
-for span in yellow_spans:
-    teachers += span.find_all('a')
 
 def judge_item(teacher):
     print('judging teacher %s...' % teacher.string)
@@ -31,7 +17,8 @@ def judge_item(teacher):
         'kcdm': '',
         'pageXnxq': ''
     }
-    r = session.post("http://10.200.21.61:7001/ieas2.1/xspj/toAddPjjs", data=form_data)
+    r = session.post("http://10.200.21.61:7001/ieas2.1/xspj/toAddPjjs", 
+                     data=form_data)
     if r.status_code != 200:
         print('failed! Details:', r.status_code, teacher_id, course_id, pjcs)
         return
@@ -50,14 +37,34 @@ def judge_item(teacher):
         if i == 0:
             option = option.find_next_sibling('input')
         form_data2[option['name']].append(option['value'])
-    r2 = session.post('http://10.200.21.61:7001/ieas2.1/xspj/saveXspj', data=form_data2)
+    r2 = session.post('http://10.200.21.61:7001/ieas2.1/xspj/saveXspj',
+                      data=form_data2)
     if r2.status_code != 200:
         print('failed! De2tails:', r2.status_code, form_data2)
         return
     print('success!')
 
 
-for teacher in teachers:
-    # print(teacher.prettify())
-    judge_item(teacher)
+def auto_judge():
+        
+    session = requests.Session()
+    session.headers = {
+        'Cookie': 'JSESSIONID=2f88b1mLwrV8Yx9SyDh1yLJQzQJzR1HF9qPCKPql63LMTXfgpnl5!-822511665'
+    }
+    res = session.get("http://10.200.21.61:7001/ieas2.1/xspj/Fxpj_fy", 
+                       allow_redirects=False)
+    if res.status_code != 200:
+        print("can't load page")
+        exit()
     
+    soup = BeautifulSoup(res.content, 'html.parser')
+    yellow_spans = soup.find_all('span', class_='yellow')
+    teachers = []
+    for span in yellow_spans:
+        teachers += span.find_all('a')
+    
+    for teacher in teachers:
+        # print(teacher.prettify())
+        judge_item(teacher)
+
+auto_judge()
