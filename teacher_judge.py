9cd4450e0550daa1a80d97c4050eae590b96c909
bearbattle <chao4150443@gmail.com>
2019-12-27 14:39:55 +0800

    修复了“登录超时，请重新登录”的错误 更改了 login_url 修改了登录成功的判定方式

--- teacher_judge.py
+++ teacher_judge.py
@@ -5,14 +5,16 @@ from collections import defaultdict
 session = requests.Session()
 jiaowu_url = 'http://10.200.21.61:7001/ieas2.1'
 # use '/login' instead of '/login/' to avoid redirects
-login_url = 'https://sso.buaa.edu.cn/login'
+login_url = 'https://sso.buaa.edu.cn/login?service=http%3A%2F%2F10.200.21.61%3A7001%2Fieas2.1%2Fwelcome'
+
 
 def get_login_token() -> str:
     r = session.get(login_url)
-    assert(r.status_code == 200)
+    assert (r.status_code == 200)
     soup = BeautifulSoup(r.content, 'html.parser')
     lt = soup.find('input', {'name': 'lt'})['value']
     return lt
+
 
 def login(username: str, password: str) -> bool:
     formdata = {
@@ -24,14 +26,15 @@ def login(username: str, password: str) -> bool:
         '_eventId': 'submit',
         'submit': '登陆'
     }
-    r2 = session.post(login_url, data=formdata, allow_redirects=False)
-    assert(r2.status_code == 200)
-    return 'Set-Cookie' in r2.headers
+    r2 = session.post(login_url, data=formdata, allow_redirects=True)
+    soup = BeautifulSoup(r2.text, "html.parser")
+    return not soup.find_all('div', class_='error_txt')
+
 
 def assess_item(teacher: "beautifulSoup object"):
     print('evaluate teacher %s...' % teacher.string)
     course_id = list(teacher.parents)[2]['rwh_id']
-    teacher_info = teacher['onclick'].split("'")   
+    teacher_info = teacher['onclick'].split("'")
     teacher_id = teacher_info[1]
     pjcs = teacher_info[3]
     form_data = {
@@ -41,9 +44,9 @@ def assess_item(teacher: "beautifulSoup object"):
         'kcdm': '',
         'pageXnxq': ''
     }
-    r = session.post(jiaowu_url + "/xspj/toAddPjjs", 
+    r = session.post(jiaowu_url + "/xspj/toAddPjjs",
                      data=form_data)
-    assert(r.status_code == 200)
+    assert (r.status_code == 200)
     s = BeautifulSoup(r.content, "html.parser")
     form = s.find('form', id='queryform')
     entries = form.find_all('input', type='hidden')
@@ -59,28 +62,31 @@ def assess_item(teacher: "beautifulSoup object"):
         form_data2[option['name']].append(option['value'])
     r2 = session.post(jiaowu_url + '/xspj/saveXspj',
                       data=form_data2)
-    assert(r2.status_code == 200)
+    assert (r2.status_code == 200)
     print('success!')
 
+
 def auto_evaluation():
-    r = session.get(jiaowu_url + '/xspj/Fxpj_fy', 
+    r = session.get(jiaowu_url + '/xspj/Fxpj_fy',
                     allow_redirects=False)
-    assert(r.status_code == 200)
+    assert (r.status_code == 200)
     soup = BeautifulSoup(r.content, 'html.parser')
     yellow_spans = soup.find_all('span', class_='yellow')
     teachers = []
     for span in yellow_spans:
-        teachers += span.find_all('a')   
+        teachers += span.find_all('a')
     for teacher in teachers:
         assess_item(teacher)
     print("all job done!")
 
+
 def auto_judge():
-	username = input('请输入统一认证登陆账号：')
-	password = input('请输入统一认证登陆密码：')
-	if login(username, password) == True:
-		auto_evaluation()
-	else:
-		print("账号或者密码错误(请确保连入校园网)")
-	
+    username = input('请输入统一认证登陆账号：')
+    password = input('请输入统一认证登陆密码：')
+    if login(username, password):
+        auto_evaluation()
+    else:
+        print("账号或者密码错误(请确保连入校园网)")
+
+
 auto_judge()
